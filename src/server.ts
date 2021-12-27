be71383aca9dfb457e8117b16b2285aaddf9af1a
Douglas Lopes dos Santos <contato.geardev@gmail.com>
2021-12-27 19:07:28 -0300

    chore: add ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { createCourse } from './routes';
 
- const app = express();
+const app = express();
+const port = process.env.PORT || 3333
 
 app.get('/', createCourse)
 
-app.listen(3333)
+app.listen(port, () => console.log(`server is running! ${port}`))
