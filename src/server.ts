fcd386b951c727ad38909e7834bfa7c390827474
Douglas Lopes dos Santos <contato.geardev@gmail.com>
2021-12-27 21:05:22 -0300

    feat: created model Category

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ const app = express();
 const port = process.env.PORT || 3333
 
 app.use(express.json());
-app.use(categoriesRoutes)
+app.use('/categories',categoriesRoutes)
 
 app.listen(port, () => console.log(`server is running! ${port}`))
