68869fbb6ae14e08545d8d7b54a17d12b328d623
Douglas Lopes dos Santos <contato.geardev@gmail.com>
2021-12-27 22:58:14 -0300

    feat: created interfaces ISpecificationsRepository and CreateSpecificationService

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specifications.routes';
 
 const app = express();
 const port = process.env.PORT || 3333
 
 app.use(express.json());
 app.use('/categories',categoriesRoutes)
+app.use('/specifications',specificationsRoutes)
 
 app.listen(port, () => console.log(`server is running! ${port}`))
