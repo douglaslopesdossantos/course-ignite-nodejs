1c7a223c35cfb51552183d2782dfefd32224795b
Douglas Lopes dos Santos <contato.geardev@gmail.com>
2021-12-27 21:22:29 -0300

    feat: created list CategoriesRepository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -14,4 +14,10 @@ categoriesRoutes.post('/', (request, response) => {
 
 })
 
+categoriesRoutes.get('/', (request, response) => {
+  const all = categoriesRepository.list()
+  return response.status(200).json(all)
+})
+
+
 export { categoriesRoutes }
