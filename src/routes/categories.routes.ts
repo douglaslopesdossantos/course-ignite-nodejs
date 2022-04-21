87a3c4a40463b0b88de1ea0c0075975c241b3a31
Douglas Lopes dos Santos <contato.geardev@gmail.com>
2022-04-21 19:46:22 -0300

    chore: add tsyringe and implementation tsyrunge

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import multer from 'multer'
 
-import  createCategoryController  from '../modules/cars/useCases/createCategory'
+import { CreateCategoryController  } from '../modules/cars/useCases/createCategory/CreateCategoryController'
+
 import  { listCategoriesController }  from '../modules/cars/useCases/listCategories'
 import  { importCategoryController }  from '../modules/cars/useCases/importCategory'
+
+const createCategoryController = new CreateCategoryController()
 
 const categoriesRoutes = Router()
 
@@ -11,7 +14,7 @@ const upload = multer({
   dest: "./tmp",
 })
 
-categoriesRoutes.post('/', (request, response) => { return createCategoryController().handle(request, response)})
+categoriesRoutes.post('/', createCategoryController.handle)
 categoriesRoutes.get('/', (request, response) => { return listCategoriesController.handle(request, response)})
 categoriesRoutes.post('/import', upload.single("file") ,(request, response) => {  importCategoryController.handle(request, response)})
 
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import './database'
 import express from 'express';
+import swaggerFile from './swagger.json'
+import './database'
+import './shared/container'
 import swaggerUi from 'swagger-ui-express'
 import { router } from './routes'
-import swaggerFile from './swagger.json'
 
 const app = express();
 const port = process.env.PORT || 3333
@@ -10,6 +11,5 @@ const port = process.env.PORT || 3333
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(router)
-
 
 app.listen(port, () => console.log(`server is running! ${port}`))
